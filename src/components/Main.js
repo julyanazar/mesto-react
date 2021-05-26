418a5b94474eba8a2a73fb7eb8de0c2da345d837
julyanazar <nnwrnuwr@gmail,com>
2021-05-26 14:03:30 +0400

    refactor: указать в качестве начального значения пустую строку

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,9 +5,9 @@ import api from '../utils/api.js';
 
 function Main({ onEditProfile, onAddPlace, onEditAvatar, onCardClick }) {
 
-    const [userName, setUserName] = React.useState();
-    const [userDescription, setUserDescription] = React.useState();
-    const [userAvatar, setUserAvatar] = React.useState();
+    const [userName, setUserName] = React.useState('');
+    const [userDescription, setUserDescription] = React.useState('');
+    const [userAvatar, setUserAvatar] = React.useState('');
     const [cards, setCards] = React.useState([]);
 
     React.useEffect(() => {

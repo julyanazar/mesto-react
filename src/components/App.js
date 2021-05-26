9b7c4ab51aa3bd18391170caf49df70da1273534
julyanazar <nnwrnuwr@gmail,com>
2021-05-26 13:27:31 +0400

    refactor: использовать корректный тип данных для selectedCard

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ function App() {
     const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false);
     const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false);
     const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = React.useState(false);
-    const [selectedCard, setSelectedCard] = React.useState(false);
+    const [selectedCard, setSelectedCard] = React.useState(null);
 
     function handleEditProfileClick() {
         setIsEditProfilePopupOpen(true);
@@ -31,7 +31,7 @@ function App() {
         setIsEditAvatarPopupOpen(false);
         setIsEditProfilePopupOpen(false);
         setIsAddPlacePopupOpen(false);
-        setSelectedCard(false);
+        setSelectedCard(null);
     }
 
     return (
--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -4,8 +4,8 @@ function ImagePopup({ card, onClose }) {
     return (
         <div className={`popup popup_type_image ${card ? 'popup_opened' : ''}`}>
             <div className="popup__container-img">
-                <img src={card.link} alt="" className="popup__img" />
-                <h3 className="popup__caption">{card.name}</h3>
+                <img src={card ? card.link : ''} alt={card ? card.name : ''} className="popup__img" />
+                <h3 className="popup__caption">{card ? card.name : ''}</h3>
                 <button className="popup__close-button" type="button" onClick={onClose} />
             </div>
         </div>
